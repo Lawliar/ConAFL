403833090240a137aa635ba43b3a3988adc3638b
Liu changming <charley.ashbringer@gmail.com>
2018-09-06 11:37:06 +0800

    Add files via upload
    initial post

---
+++ compile-run.py
@@ -0,0 +1,23 @@
+import os
+import sys
+def main(argv):
+  files = ''
+  print argv
+  for eachfile in argv:
+    files += eachfile + ' '
+  dirname = os.path.dirname(argv[0])
+  thisDirname = os.path.dirname(__file__) 
+  cmd = os.path.join(thisDirname,'afl-gcc')+ ' -pthread -g ' + files + '-o '+os.path.join(dirname,'executable')
+  os.system(cmd)
+  assert(os.path.exists(os.path.join(dirname,'conafl.txt')))#means compile success
+  cmd = 'rm -f '+os.path.join(dirname,'conafl.txt')
+  os.system(cmd)
+  if os.path.exists(os.path.join(dirname,'out')):
+    cmd = 'rm -rf '+os.path.join(dirname,'out')
+    os.system(cmd)
+    os.mkdir(os.path.join(dirname,'out'))
+  assert(os.path.exists(os.path.join(dirname,'in')))
+  cmd =os.path.join(thisDirname,'afl-fuzz')+ ' -m 1GB -i ' + os.path.join(dirname,'in') + ' -o' + os.path.join(dirname,'out') + ' -- ' + os.path.join(dirname,'executable') + ' @@'
+  os.system(cmd)
+if __name__ == '__main__':
+  main(sys.argv[1:])
