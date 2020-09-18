7560f06af03e58175a0b817ede1f149797c5ef92
Changming Liu <charley.ashbringer@gmail.com>
2020-09-17 20:21:26 -0400

    fix ad-hoc error in the python script

--- compile-run.py
+++ compile-run.py
@@ -2,12 +2,14 @@ import os
 import sys
 def main(argv):
   files = ''
-  print argv
+  print(argv)
   for eachfile in argv:
     files += eachfile + ' '
   dirname = os.path.dirname(argv[0])
-  thisDirname = os.path.dirname(__file__) 
+  thisDirname = os.path.dirname(os.path.realpath(__file__)) 
+  sys.path.append(thisDirname)
   cmd = os.path.join(thisDirname,'afl-gcc')+ ' -pthread -g ' + files + '-o '+os.path.join(dirname,'executable')
+  print(cmd)
   os.system(cmd)
   assert(os.path.exists(os.path.join(dirname,'conafl.txt')))#means compile success
   cmd = 'rm -f '+os.path.join(dirname,'conafl.txt')
@@ -16,7 +18,8 @@ def main(argv):
     cmd = 'rm -rf '+os.path.join(dirname,'out')
     os.system(cmd)
     os.mkdir(os.path.join(dirname,'out'))
-  assert(os.path.exists(os.path.join(dirname,'in')))
+  if not os.path.exists(os.path.join(dirname,'in')):
+    os.mkdir(os.path.join(dirname,'in'))
   cmd =os.path.join(thisDirname,'afl-fuzz')+ ' -m 1GB -i ' + os.path.join(dirname,'in') + ' -o' + os.path.join(dirname,'out') + ' -- ' + os.path.join(dirname,'executable') + ' @@'
   os.system(cmd)
 if __name__ == '__main__':
